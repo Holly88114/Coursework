4776d94e791e965657b2937f502338d51d111828
Holly88114 <88114@farnborough.ac.uk>
2019-12-13 12:02:40 +0000

    Phase 3 login kinda pt 2

--- resources/client/js/index.js
+++ resources/client/js/index.js
@@ -6,12 +6,14 @@ function pageLoad() {
         document.getElementById("loginButton").addEventListener("click", login);
     }
 
+
+
     function login(event) {
-        evemt.preventDefault();
+        event.preventDefault();
         const form = document.getElementById("loginForm");
         const formData = new FormData(form);
 
-        fetch("/user/login", {method: 'post', body: formData}
+        fetch("/student/login", {method: 'post', body: formData}
         ).then(response => response.json()
         ).then(responseData => {
             if (responseData.hasOwnProperty('error')) {
@@ -19,7 +21,6 @@ function pageLoad() {
             } else {
                 Cookies.set("username", responseData.username);
                 Cookies.set("token", responseData.token);
-                window.location.href = '/client/index.html'
             }
         });
     }
